99f704d10517ba65bd2461171903c5e2d1a56603
Lucas Faria <lucasfaria@outlook.pt>
2024-04-14 19:06:57 +0100

    Started working on the classes that are related to the database

---
+++ database/product.class.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+class Product{
+
+    private int $id;
+    private int $price;
+    private int $quantity;
+    private string $name;
+    private string $description;
+    private int $owner;
+    private int $category;
+    private int $brand;
+
+}
---
+++ database/user.class.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types = 1);
+class User{
+
+    private int $id;
+    private string $username;
+    private string $email;
+    private int $isadmin;
+
+    public function __construct(int $id, string $username, string $email, int $isadmin){
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->isadmin = $isadmin;
+    }
+
+    function getUsername() : string{
+        return $this->username;
+    }
+
+    function getEmail() : string{
+        return $this->email;
+    }
+
+    function getIsAdmin() : int{
+        return $this->isadmin;
+    }
+
+    function getUserOwnedItens(PDO $db){
+        $stmt = $db->prepare('
+            SELECT *
+            FROM PRODUCTS
+            WHERE owner = ?
+            ');
+        
+        $stmt->fetchAll();
+    }
+
+    static function getUser(PDO $db, int $id){
+        $stmt = $db->prepare('
+            SELECT id, username, email, is_admin
+            FROM USERS
+            WHERE id = ?
+            ');
+        
+        $stmt->execute(array($id));
+        $user = $stmt->fetch();
+
+        return new User(
+            $user['id'],
+            $user['username'],
+            $user['email'],
+            $user['isadmin']
+        );
+
+    }
+
+
+
+
+
+}
