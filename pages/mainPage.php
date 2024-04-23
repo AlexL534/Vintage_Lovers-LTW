4b1c7ed8b0b239fa50f97a095dd12d6adbd6852c
AlexL534 <alexandrefernadeslopes@gmail.com>
2024-04-23 15:35:07 +0100

    created a folder for classes for better organization

--- pages/mainPage.php
+++ pages/mainPage.php
@@ -2,7 +2,7 @@
 
 require_once(__DIR__ . '/../templates/common.tpl.php');
 require_once(__DIR__ . '/../database/database_connection.db.php');
-require_once(__DIR__ . '/../database/product.class.php');
+require_once(__DIR__ . '/../classes/product.class.php');
 require_once(__DIR__ . '/../utils/sessions.php');
 $db = getDatabaseConnection();
 $session = new Session();
--- pages/profile.php
+++ pages/profile.php
@@ -3,7 +3,7 @@
 require_once(__DIR__ . '/../utils/sessions.php');
 require_once(__DIR__ . '/../templates/common.tpl.php');
 require_once(__DIR__ . '/../templates/profile.tpl.php');
-require_once(__DIR__ . '/../database/user.class.php');
+require_once(__DIR__ . '/../classes/user.class.php');
 require_once(__DIR__ . '/../database/database_connection.db.php');
 
 $session = new Session();
