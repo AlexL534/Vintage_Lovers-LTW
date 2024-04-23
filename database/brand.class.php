91a8cd2b818152735e049d7f05eef0a9ca97be21
Rafael Campeão <up202207553@up.pt>
2024-04-23 18:49:23 +0100

    added classes for categoty, brand, color, size and condition

---
+++ database/brand.class.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+
+class Brand{
+    private int $id;
+    private string $name;
+
+    public function __construct(int $id, string $name){
+        $this->id= $id;
+        $this->name= $name;
+
+    }
+
+
+    //getters
+    public function getId() : int{
+        return $this->id;
+    }
+
+    public function getName() : string{
+        return $this->name;
+    }
+
+    //querys
+    static public function getBrandById(PDO $db, int $id){
+        $stmt = $db->prepare('SELECT * FROM BRAND WHERE id = ?');
+        $stmt->execute(array($id));
+        $brandDB = $stmt->fetch(); 
+
+        return new Brand(
+            intval($brandDB['id']),
+            $brandDB['name']
+        );
+    }
+
+    static public function getBrandByName(PDO $db, string $name){
+        $stmt = $db->prepare('SELECT * FROM BRAND WHERE name = ?');
+        $stmt->execute(array($name));
+        $brandDB = $stmt->fetch();
+        
+        return new Brand(
+            intval($brandDB['id']),
+            $brandDB['name']
+        );
+    }
+
+    static public function getAllBrands(PDO $db){
+        $stmt = $db->prepare('SELECT * FROM BRAND');
+        $stmt->execute();
+        $brands = array();
+        while($brandDB = $stmt->fetch()){
+            $brand= new Brand(intval($brandDB['id']),$brandDB['name']);
+
+            $brands[]=$brand;
+        }
+
+        return $brands;
+    }
+}
+?>
