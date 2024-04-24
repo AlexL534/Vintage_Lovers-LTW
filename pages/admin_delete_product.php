78fc5f89ee970bd9c8b2598f7b5145f4545b23dc
AlexL534 <alexandrefernadeslopes@gmail.com>
2024-04-24 13:02:44 +0100

    now admin_pages can only be accesses if the user logged in and is admin

--- pages/admin_delete_product.php
+++ pages/admin_delete_product.php
@@ -2,8 +2,20 @@
 
 require_once(__DIR__ . '/../templates/common.tpl.php');
 require_once(__DIR__ . '/../templates/admin.php');
+require_once(__DIR__ . '/../utils/sessions.php');
+require_once(__DIR__ . '/../classes/user.class.php');
+require_once(__DIR__ . '/../database/database_connection.db.php');
 
-drawHeader();
+$db = getDatabaseConnection();
+$session = new Session();
+$admin = User::getUser($db, $session->getId())->getIsAdmin();
+
+if (!$session->isLoggedIn() || !$admin) {
+    header("Location: /"); 
+    exit; 
+}
+
+drawHeader2($session);
 drawProductList(true); //according to figma, it should have product image, but database still doesn't support that
 drawFooter(); //also, didn't implement check info button (don't know what info should be displayed) nor delete
 
--- pages/admin_specific_filter.php
+++ pages/admin_specific_filter.php
@@ -1,8 +1,20 @@
 <?php
 require_once(__DIR__ . '/../templates/common.tpl.php');
 require_once(__DIR__ . '/../templates/admin.php');
+require_once(__DIR__ . '/../utils/sessions.php');
+require_once(__DIR__ . '/../classes/user.class.php');
+require_once(__DIR__ . '/../database/database_connection.db.php');
 
-drawHeader();
+$db = getDatabaseConnection();
+$session = new Session();
+$admin = USER::getUser($db,$session->getId())->getIsAdmin();
+
+if (!$session->isLoggedIn() || !$admin) {
+    header("Location: /"); 
+    exit; 
+}
+
+drawHeader2($session);
 
 if(isset($_GET['type'])) {
     $type = $_GET['type'];
