34326e22e5ccbf4be6c549596b298b103702894d
Rafael Campeão <up202207553@up.pt>
2024-05-09 02:16:05 +0100

    added the UserMessage class

---
+++ templates/messages.tpl.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types = 1);
+require_once(__DIR__ . '/../classes/product.class.php');
+require_once(__DIR__ . '/../classes/session.class.php');
+require_once(__DIR__ . '/../classes/user.class.php');
+
+?>
